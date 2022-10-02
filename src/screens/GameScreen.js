8ef8f720f5a14aff5e837e5a8c63f94bf2d87f23
biancaclee <rm89171@fiap.com.br>
2022-10-02 01:53:44 -0300

    Criando slider para tela Game

--- src/screens/GameScreen.js
+++ src/screens/GameScreen.js
@@ -1,3 +1,4 @@
+
 import { useEffect, useState } from "react";
 import {
   StyleSheet,
@@ -9,91 +10,83 @@ import {
   Dimensions,
 } from "react-native";
 import RatingComponent from "../Components/RatingComponent";
+import AppIntroSlider from 'react-native-app-intro-slider';
 
+const slides = [
+  {
+    key: '1',
+    title: 'Teste 1',
+    text: 'Texto do teste 1',
+    text2: 'Teste 1930257835903284',
+    //image: require('../../assets/logo.png'),
+  },
+  {
+    key: '2',
+    title: 'Teste 2',
+    text: 'Texto do teste 2',
+  },
+  {
+    key: '3',
+    title: 'Teste 3',
+    text: 'Texto do teste 3',
+  },
+];
 
-// import Carousel from "react-native-snap-carousel"
-
-// const sliderWidth = Dimensions.get('window').width
-// const itemWidth = sliderWidth * 0.88
-
-// type Props = {
-//   item: { imgUrl: string }
-//   index: number
-// }
-
-// const carouselItems = [
-//   {
-//     imgUrl: 'https://images.unsplash.com/photo-1501003878151-d3cb87799705?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80'
-//   },
-//   {
-//     imgUrl: 'https://images.unsplash.com/photo-1617783919141-8417358d4e29?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80'
-//   },
-//   {
-//     imgUrl: 'https://images.unsplash.com/photo-1640461470346-c8b56497850a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80'
-//   }
-// ]
-
-
-// function carouselCardItem({item, index}: Props) {
-//   return(
-//     <View style={styles.cardCarousel} key={index}>
-//       <Image style={styles.image} source={{ url: item.imgUrl }} />
-//     </View>
-//   )
-// }
-
-// export function GameScreen() {
-//     return(
-//         <ImageBackground
-//           source={require("../../assets/bg.png")}
-//           blurRadius={3}
-//           style={styles.imgBackground}
-//         >
-//           <ImageBackground
-//             style={styles.imgGameBackground}
-//             source={{
-//               uri: "https://images.unsplash.com/photo-1501003878151-d3cb87799705?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
-//             }}
-//             blurRadius={5}
-//           >
-//             <Text style={styles.gameTitle}>JOGO X</Text>
-//           </ImageBackground>
-
-//           <View style={styles.carousel}>
-//             <Carousel
-//               data={carouselItems}
-//               renderItem={carouselCardItem}
-//               sliderWidth={sliderWidth}
-//               itemWidth={itemWidth}
-//               useScrollView={true}
-//             />
-//           </View>
-
-
-//         </ImageBackground>
-
-
-//     )
-// }
 
 export default function GameScreen() {
-    return(
-        <ImageBackground
-          source={require("../../assets/bg.png")}
-          blurRadius={3}
-          style={styles.imgBackground}
-        >
-          <ImageBackground
-            style={styles.imgGameBackground}
-            source={{
-              uri: "https://images.unsplash.com/photo-1501003878151-d3cb87799705?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
-            }}
-            blurRadius={5}
-          >
-            <Text style={styles.gameTitle}>JOGO X</Text>
-          </ImageBackground>
+  const [showHome, setShowHome] = useState(false);
 
-          <View style={styles.carousel}>
+  function renderSlides({ item }){
+    return(
+      <View style={{flex:1}}>
+        <Text>{item.title}</Text>
+        <Text>{item.text}</Text>
+        <Text>{item.text2}</Text>
+        {/*<Image source={item.image} />*/}
+      </View>
+    )
+  }
+
+  if(showHome){
+    return <Text>Teste entrou na home</Text>
+  } else {
+    return(
+      <ImageBackground
+        source={require("../../assets/bg.png")}
+        blurRadius={3}
+        style={styles.imgBackground}
+      >
+        <ImageBackground
+          style={styles.imgGameBackground}
+          source={{
+            uri: "https://images.unsplash.com/photo-1501003878151-d3cb87799705?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+          }}
+          blurRadius={5}
+        >
+          <Text style={styles.gameTitle}>JOGO X</Text>
+        </ImageBackground>
+
+        <AppIntroSlider
+          renderItem={renderSlides}
+          data={slides}
+          activeDotStyle={{
+            backgroundColor: '#FFFF00',
+            width: 30,
+          }}
+        />
+
+        
+
+        {/* <ScrollView
+          horizontal={true}
+          contentContainerStyle={{ width: '${100}%' }}
+          // showsHorizontalScrollIndicator={false}
+          scrollEventThrottle={100}
+          decelerationRate="fast"
+          pagingEnabled
+        > */}
+
+          {/* <View style={styles.carousel}>
             <View style={styles.informacoes}>
               <Text style={styles.textTitle}>Categoria:</Text>
               <Text style={styles.textInfo}>Cartas</Text>
@@ -102,19 +95,25 @@ export default function GameScreen() {
               <Text style={styles.textTitle}>Quantidade de Jogadores:</Text>
               <Text style={styles.textInfo}>2 - 6 jogadores</Text>
               <Text style={styles.textTitle}>Avaliação do Jogo:</Text>
-              <Text style={styles.textInfo}>4,5
+              <Text style={styles.textInfo}>4,5 */}
                 {/* <RatingComponent 
                   score ={5}
                 /> */}
-              </Text>
+
+              {/* </Text>
             </View>
-          </View>
+          </View> */}
+
+        {/* </ScrollView> */}
 
 
-        </ImageBackground>
+      </ImageBackground>
+  
 
 
-    )
+
+    );
+  }
 }
 
 
